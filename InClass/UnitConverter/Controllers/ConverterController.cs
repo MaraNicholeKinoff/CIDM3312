c36b987ecbeef2b06d741b5447663131fac1402f
Mara Kinoff <mk962071@wtstudents.wtamu.edu>
2018-10-29 11:59:15 -0500

    Converter

---
+++ InClass/UnitConverter/Controllers/ConverterController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UnitConverter.Models;
+
+namespace UnitConverter.Controllers
+{
+    public class ConverterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
